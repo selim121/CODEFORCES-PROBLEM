5629facfa54e268b216fe5ecaedcc3c565e00ad8
selim121 <selimkhan.bdkhan@gmail.com>
2022-11-16 19:52:10 +0600

    1748A

---
+++ 1748A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int t,n,b;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        b = (n+1)/2;
+        cout<<b<<endl;
+    }
+    return 0;
+}
