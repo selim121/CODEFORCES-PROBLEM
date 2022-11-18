8ad85e3882711dee582d2cb0742f01093fd0bbaf
selim121 <selimkhan.bdkhan@gmail.com>
2022-11-18 20:01:07 +0600

    1741B

---
+++ 1741B.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n==3){
+            cout<<"-1";
+        }else{
+        cout<<n<<" "<<n-1<<" ";
+        for(int i=1;i<n-1;i++){
+            cout<<i<<" ";
+        }
+        }
+        cout<<endl;
+    }
+	return 0;
+}
